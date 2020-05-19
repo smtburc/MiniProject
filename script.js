7a13ef961ea80cb156b7b27ee5c8fffa24712c98
smtburc <sametburc@gmail.com>
2020-05-19 10:02:34 +0300

    Rainbow Filter

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function uploadFile() {
     image.drawTo(canvas);
 }
 
+function imageIsLoaded() {
+    return image != null;
+}
+
+function resetImage() {
+    image.drawTo(canvas);
+}
+
+//doMethots
+
 function doGray() {
     if (imageIsLoaded()) {     // check if image is loaded
         filterGray();	                      // function performs the grayscale work
@@ -17,9 +27,28 @@ function doGray() {
     }
 }
 
-function imageIsLoaded() {
-    return image != null;
+function doRed() {
+    if (imageIsLoaded()) {     // check if image is loaded
+        filterRed();	                      // function performs the grayscale work
+        filteredImage.drawTo(canvas);	          // display image
+    }
 }
+
+function doGrayCenteredRed() {
+    if (imageIsLoaded()) {     // check if image is loaded
+        filterGrayCenteredRed();	                      // function performs the grayscale work
+        filteredImage.drawTo(canvas);	          // display image
+    }
+}
+
+function doRainbow() {
+    if (imageIsLoaded()) {     // check if image is loaded
+        filterRainbow();	                      // function performs the grayscale work
+        filteredImage.drawTo(canvas);	          // display image
+    }
+}
+
+//filter methots
 
 function filterGray() {
     filteredImage = new SimpleImage(image.getWidth(), image.getHeight());
@@ -28,24 +57,8 @@ function filterGray() {
         var y = pixel.getY();
         var filteredPixel = filteredImage.getPixel(x, y);
         var avarage = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-        filteredPixel.setRed(avarage);
-        filteredPixel.setGreen(avarage);
-        filteredPixel.setBlue(avarage);
+        toGray(filteredPixel, avarage);
     }
-}
-
-function resetImage() {
-    image.drawTo(canvas);
-
-}
-
-function doRed() {
-
-    if (imageIsLoaded()) {     // check if image is loaded
-        filterRed();	                      // function performs the grayscale work
-        filteredImage.drawTo(canvas);	          // display image
-    }
-
 }
 
 function filterRed() {
@@ -55,23 +68,7 @@ function filterRed() {
         var y = pixel.getY();
         var filteredPixel = filteredImage.getPixel(x, y);
         var avarage = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-        if (avarage < 128) {
-            filteredPixel.setRed(avarage * 2);
-            filteredPixel.setGreen(0);
-            filteredPixel.setBlue(0);
-        } else {
-            filteredPixel.setRed(255);
-            filteredPixel.setGreen(avarage * 2 - 255);
-            filteredPixel.setBlue(avarage * 2 - 255);
-        }
-    }
-}
-
-
-function doGrayCenteredRed() {
-    if (imageIsLoaded()) {     // check if image is loaded
-        filterGrayCenteredRed();	                      // function performs the grayscale work
-        filteredImage.drawTo(canvas);	          // display image
+        toRed(filteredPixel, avarage);
     }
 }
 
@@ -89,20 +86,136 @@ function filterGrayCenteredRed() {
 
         var line = Math.random() * r;
         if (Math.sqrt(Math.pow((x - centerX), 2) + Math.pow((y - centerY), 2)) > line) {
-            if (avarage < 128) {
-                filteredPixel.setRed(avarage * 2);
-                filteredPixel.setGreen(0);
-                filteredPixel.setBlue(0);
-            } else {
-                filteredPixel.setRed(255);
-                filteredPixel.setGreen(avarage * 2 - 255);
-                filteredPixel.setBlue(avarage * 2 - 255);
-            }
+            toRed(filteredPixel, avarage);
         } else {
-            filteredPixel.setRed(avarage);
-            filteredPixel.setGreen(avarage);
-            filteredPixel.setBlue(avarage);
+            toGray(filteredPixel, avarage);
         }
     }
 
+}
+
+function filterRainbow() {
+    filteredImage = new SimpleImage(image.getWidth(), image.getHeight());
+    for (var pixel of image.values()) {
+        var x = pixel.getX();
+        var y = pixel.getY();
+        var filteredPixel = filteredImage.getPixel(x, y);
+        var avarage = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+        if (y < image.getHeight() / 7) {
+            toRed(filteredPixel, avarage);
+
+        } else if (y < image.getHeight() / 7 * 2) {
+            toOrange(filteredPixel, avarage);
+
+        } else if (y < image.getHeight() / 7 * 3) {
+            toYellow(filteredPixel, avarage);
+
+        } else if (y < image.getHeight() / 7 * 4) {
+            toGreen(filteredPixel, avarage);
+
+        } else if (y < image.getHeight() / 7 * 5) {
+            toBlue(filteredPixel, avarage);
+
+        } else if (y < image.getHeight() / 7 * 6) {
+            toIndigo(filteredPixel, avarage);
+
+        } else {
+            toViolet(filteredPixel, avarage);
+
+        }
+    }
+}
+
+//color methots
+
+function toRed(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(avarage * 2);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+    } else {
+        pixel.setRed(255);
+        pixel.setGreen(avarage * 2 - 255);
+        pixel.setBlue(avarage * 2 - 255);
+    }
+}
+
+function toGray(pixel, avarage) {
+    pixel.setRed(avarage);
+    pixel.setGreen(avarage);
+    pixel.setBlue(avarage);
+}
+
+function toOrange(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(avarage * 2);
+        pixel.setGreen(avarage * 0.8);
+        pixel.setBlue(0);
+    } else {
+        pixel.setRed(255);
+        pixel.setGreen(avarage * 1.2 - 51);
+        pixel.setBlue(avarage * 2 - 255);
+    }
+}
+
+function toYellow(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(avarage * 2);
+        pixel.setGreen(avarage * 2);
+        pixel.setBlue(0);
+    } else {
+        pixel.setRed(255);
+        pixel.setGreen(255);
+        pixel.setBlue(avarage * 2 - 255);
+    }
+}
+
+function toGreen(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(avarage * 2);
+        pixel.setBlue(0);
+    } else {
+        pixel.setRed(avarage * 2 - 255);
+        pixel.setGreen(255);
+        pixel.setBlue(avarage * 2 - 255);
+    }
+}
+
+function toBlue(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(avarage * 2);
+    } else {
+        pixel.setRed(avarage * 2 - 255);
+        pixel.setGreen(avarage * 2 - 255);
+        pixel.setBlue(255);
+    }
+}
+
+
+function toIndigo(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(avarage * 0.8);
+        pixel.setGreen(0);
+        pixel.setBlue(avarage * 2);
+    } else {
+        pixel.setRed(avarage * 1.2 - 51);
+        pixel.setGreen(avarage * 2 - 255);
+        pixel.setBlue(255);
+    }
+}
+
+
+function toViolet(pixel, avarage) {
+    if (avarage < 128) {
+        pixel.setRed(avarage * 1.6);
+        pixel.setGreen(0);
+        pixel.setBlue(avarage * 1.6);
+    } else {
+        pixel.setRed(avarage * 0.4 + 153);
+        pixel.setGreen(avarage * 2 - 255);
+        pixel.setBlue(avarage * 0.4 + 153);
+    }
 }
